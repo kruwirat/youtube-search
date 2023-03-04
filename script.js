9699988c6980d604f98d487b8ddf711919aadbfb
kruwirat <113408113+kruwirat@users.noreply.github.com>
2023-03-04 22:15:58 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("btn").addEventListener('click',search)
+function search(){
+    event.preventDefault()
+    var name = document.getElementById("keyword").value
+    window.open("https://www.youtube.com/@sompongpokasri5783/search?query="+name, '_blank','location=yes,height=840,width=580,scrollbars=yes,status=yes'
+    )
+} 
